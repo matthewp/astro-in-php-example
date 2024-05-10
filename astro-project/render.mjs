596bc15335f71f0cd3e4b47132efe06c4ef40e7f
Matthew Phillips <matthew@skypack.dev>
2024-05-10 08:38:07 -0400

    Clean up

--- astro-project/render.mjs
+++ astro-project/render.mjs
@@ -2,9 +2,7 @@ import * as components from './dist/server/all.mjs';
 import { renderers } from './dist/server/renderers.mjs';
 import { unstable_AstroContainer as AstroContainer } from 'astro/container';
 
-const container = await AstroContainer.create({
-  renderers: renderers
-});
+const container = await AstroContainer.create({ renderers });
 const html = await container.renderToString({ default: components.ReactWrapper });
 
 console.log(html);
